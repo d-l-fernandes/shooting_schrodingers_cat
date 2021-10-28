9eed53de2755e3aee18e14c94930a83764f83f34
David Fernandes <dffernandes95@gmail.com>
2021-10-28 12:59:27 +0100

    Added score network

--- sde/drifts.py
+++ sde/drifts.py
@@ -1,5 +1,7 @@
 import torch
 from absl import flags
+import math
+import torch.nn.functional as F
 
 Tensor = torch.Tensor
 
@@ -10,6 +12,7 @@ flags.DEFINE_enum("drift", "nn_space",
                    "nn_space_mnist",
                    "nn_general",
                    "nn_general_mnist",
+                   "score_network"
                    ],
                   "Drift to use.")
 FLAGS = flags.FLAGS
@@ -79,7 +82,8 @@ class NNGeneral(BaseDrift):
         super().__init__(input_size, output_size)
         intermediate_size = 20 * output_size
         self.nn = torch.nn.Sequential(
-            torch.nn.Linear(self.input_size+1, intermediate_size), torch.nn.SiLU(),
+            torch.nn.Linear(self.input_size + 1, intermediate_size), torch.nn.SiLU(),
+            torch.nn.Linear(intermediate_size, intermediate_size), torch.nn.SiLU(),
             torch.nn.Linear(intermediate_size, intermediate_size), torch.nn.SiLU(),
             torch.nn.Linear(intermediate_size, self.output_size)
         )
@@ -96,7 +100,7 @@ class NNGeneralMNIST(BaseDrift):
         super().__init__(input_size, output_size)
         intermediate_size = 100
         self.nn = torch.nn.Sequential(
-            torch.nn.Linear(self.input_size+1, intermediate_size), torch.nn.SiLU(),
+            torch.nn.Linear(self.input_size + 1, intermediate_size), torch.nn.SiLU(),
             torch.nn.Linear(intermediate_size, intermediate_size), torch.nn.SiLU(),
             torch.nn.Linear(intermediate_size, self.output_size)
         )
@@ -108,11 +112,98 @@ class NNGeneralMNIST(BaseDrift):
         return self.nn(x)
 
 
+class ScoreNetwork(torch.nn.Module):
+
+    # def __init__(self, encoder_layers=None, pos_dim=16, decoder_layers=None, x_dim=2):
+    def __init__(self, input_size: int, output_size: int):
+        super().__init__()
+        decoder_layers = [128, 128]
+        encoder_layers = [16]
+        pos_dim = 16
+        self.temb_dim = pos_dim
+        t_enc_dim = pos_dim * 2
+        self.locals = [encoder_layers, pos_dim, decoder_layers, input_size]
+
+        self.net = MLP(2 * t_enc_dim,
+                       layer_widths=decoder_layers + [output_size],
+                       activate_final=False,
+                       activation_fn=torch.nn.SiLU())
+
+        self.t_encoder = MLP(pos_dim,
+                             layer_widths=encoder_layers + [t_enc_dim],
+                             activate_final=False,
+                             activation_fn=torch.nn.SiLU())
+
+        self.x_encoder = MLP(input_size,
+                             layer_widths=encoder_layers + [t_enc_dim],
+                             activate_final=False,
+                             activation_fn=torch.nn.SiLU())
+
+    def forward(self, x, t):
+        if len(x.shape) == 1:
+            x = x.unsqueeze(0)
+        if len(t.shape) != len(x.shape):
+            t = (torch.ones_like(x, device=x.device) * t)[..., 0:1]
+
+        temb = get_timestep_embedding(t.to(x.device), self.temb_dim)
+        temb = self.t_encoder(temb)
+        xemb = self.x_encoder(x)
+        h = torch.cat([xemb, temb], -1)
+        out = self.net(h)
+        return out
+
+
+class MLP(torch.nn.Module):
+    def __init__(self, input_dim, layer_widths, activate_final=False, activation_fn=F.relu):
+        super(MLP, self).__init__()
+        layers = []
+        prev_width = input_dim
+        for layer_width in layer_widths:
+            layers.append(torch.nn.Linear(prev_width, layer_width))
+            # # same init for everyone
+            # torch.nn.init.constant_(layers[-1].weight, 0)
+            prev_width = layer_width
+        self.input_dim = input_dim
+        self.layer_widths = layer_widths
+        self.layers = torch.nn.ModuleList(layers)
+        self.activate_final = activate_final
+        self.activation_fn = activation_fn
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers[:-1]):
+            x = self.activation_fn(layer(x))
+        x = self.layers[-1](x)
+        if self.activate_final:
+            x = self.activation_fn(x)
+        return x
+
+
+def get_timestep_embedding(timesteps, embedding_dim=128):
+    """
+      From Fairseq.
+      Build sinusoidal embeddings.
+      This matches the implementation in tensor2tensor, but differs slightly
+      from the description in Section 3.5 of "Attention Is All You Need".
+      https://github.com/pytorch/fairseq/blob/master/fairseq/modules/sinusoidal_positional_embedding.py
+    """
+    half_dim = embedding_dim // 2
+    emb = math.log(10000) / (half_dim - 1)
+    emb = torch.exp(torch.arange(half_dim, dtype=torch.float, device=timesteps.device) * -emb)
+
+    emb = timesteps.float() * emb.unsqueeze(0)
+    emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=-1)
+    if embedding_dim % 2 == 1:  # zero pad
+        emb = F.pad(emb, [0, 1])
+
+    return emb
+
+
 drifts_dict = {
     "constant": Constant,
     "linear": Linear,
     "nn_space": NNSpace,
     "nn_space_mnist": NNSpaceMNIST,
     "nn_general": NNGeneral,
-    "nn_general_mnist": NNGeneralMNIST
+    "nn_general_mnist": NNGeneralMNIST,
+    "score_network": ScoreNetwork
 }
