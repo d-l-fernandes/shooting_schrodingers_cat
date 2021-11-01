43e72d5282a50fdd25b2726deb0f3743bc0b0ccd
David Fernandes <dffernandes95@gmail.com>
2021-11-01 09:52:50 +0000

    Updated experiments for cluster

--- experiments/experiment1.sh
+++ experiments/experiment1.sh
@@ -1,16 +1,18 @@
 cd ..
-python main.py \
-  --drift=score_network \
-  --diffusion=scalar \
-  --dataset=toy_experiment_blobs_2d \
-  --prior=gaussian \
-  --prior_sde=brownian \
-  --prior_dist=gaussian \
-  --batch_size=50 \
-  --num_epochs=30 \
-  --eval_frequency=80 \
-  --learning_rate=1e-3\
-  --num_steps=10 \
-  --delta_t=0.05 \
-  --num_iter=40 \
-  --sigma=0.001 \
+hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --gpus '"device=0,1"' dlf28/pytorch_lightning\
+	python main.py \
+	  --drift=score_network \
+	  --diffusion=scalar \
+	  --dataset=toy_experiment_blobs_2d \
+	  --prior=gaussian \
+	  --prior_sde=brownian \
+	  --prior_dist=gaussian \
+	  --batch_size=1500 \
+	  --num_epochs=30 \
+	  --eval_frequency=100 \
+	  --learning_rate=1e-2\
+	  --num_steps=10 \
+	  --delta_t=0.05 \
+	  --num_iter=50 \
+	  --sigma=0.001 \
+	  --gpus=2
