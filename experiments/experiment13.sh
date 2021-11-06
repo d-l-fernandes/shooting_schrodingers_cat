843f93ef7a43087c968430c781607ed85a9852ab
David Fernandes <dffernandes95@gmail.com>
2021-11-06 21:31:09 +0000

    Changed mean to be multiplied by sigma**2

--- experiments/experiment13.sh
+++ experiments/experiment13.sh
@@ -20,5 +20,4 @@ hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime
     --num_iter=100 \
     --sigma=0.001 \
     --max_gamma=0.3 \
-    -r \
     --gpus="$number_gpus"
