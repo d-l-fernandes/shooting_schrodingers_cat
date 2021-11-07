8e1d35c48c5fd01fff9cddf7e3de42deecafba7c
David Fernandes <dffernandes95@gmail.com>
2021-11-07 11:15:35 +0000

    Changed median

--- stein/kernel.py
+++ stein/kernel.py
@@ -30,7 +30,7 @@ def inverse_exponential_decay_max_alpha(alpha, ipfp_iteration, num_epochs):
 
 
 def linear(ipfp_iteration, num_epochs):
-    return min(ipfp_iteration / 10, 1.)
+    return min(ipfp_iteration / 5, 1.)
 
 
 def constant(ipfp_iteration, num_epochs):
@@ -57,11 +57,11 @@ def stein_discrepancy(theta: Tensor, p_grad: Tensor, sigma: float, delta_t: Tens
 
     indices = torch.triu_indices(theta.shape[-2], theta.shape[-2], 1)
     h = pairwise_dists[..., indices[0], indices[1]].median(dim=-1)[0]
-    h = torch.sqrt(
-        sigma * h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
+    h = sigma * torch.sqrt(
+        h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
     # h = torch.sqrt(h).unsqueeze(-1).unsqueeze(-1)
 
-    kxy = torch.exp(-pairwise_dists / h**2 / 2) * delta_t ** 2 / sigma
+    kxy = torch.exp(-pairwise_dists / h**2 / 2) * delta_t ** 2 # / sigma
 
     h = h.unsqueeze(-1)
     dxdkxy = - 1 / h**2 * torch.einsum("...bcd,...bc->...bcd", diffs, kxy)
