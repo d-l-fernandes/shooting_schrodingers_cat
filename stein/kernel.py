899c7c53bbc8e087bf95967d21b1c8abf794f3e2
David Fernandes <dffernandes95@gmail.com>
2021-11-07 13:23:46 +0000

    Cleaned up code

--- stein/kernel.py
+++ stein/kernel.py
@@ -59,7 +59,7 @@ def stein_discrepancy(theta: Tensor, p_grad: Tensor, sigma: float, delta_t: Tens
     h = pairwise_dists[..., indices[0], indices[1]].median(dim=-1)[0]
     # h = torch.sqrt(
     #     sigma * h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
-    h = sigma * torch.sqrt(h).unsqueeze(-1).unsqueeze(-1)
+    h = sigma**0.5 * torch.sqrt(h).unsqueeze(-1).unsqueeze(-1)
 
     kxy = torch.exp(-pairwise_dists / h**2 / 2) * delta_t ** 2 / sigma**2
 
