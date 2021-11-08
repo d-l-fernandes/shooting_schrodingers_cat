ce410ff6d7dcfacc0869a0826c1ec930cb5a2cf3
David Fernandes <dffernandes95@gmail.com>
2021-11-08 19:06:56 +0000

    Back to unbiased ksd

--- stein/kernel.py
+++ stein/kernel.py
@@ -60,7 +60,7 @@ def stein_discrepancy(theta: Tensor, p_grad: Tensor, sigma: float, delta_t: Tens
     # h = torch.sqrt(
     #     sigma * h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
     h = torch.sqrt(
-        0.01 * h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1).detach()
+        h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1).detach()
     # h = torch.sqrt(delta_t * h).unsqueeze(-1).unsqueeze(-1)
 
     kxy = torch.exp(-pairwise_dists / h**2 / 2)
