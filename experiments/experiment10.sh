269c411769904d9d55ca63998ae2adc1eadf8296
David Fernandes <dffernandes95@gmail.com>
2021-11-08 10:47:19 +0000

    Constant schedule activated after first ipfp iteration

--- experiments/experiment10.sh
+++ experiments/experiment10.sh
@@ -14,7 +14,6 @@ hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime
 	  --prior_dist=gaussian \
 	  --batch_size=1500 \
 	  --num_epochs=30 \
-	  --eval_frequency=300 \
 	  --learning_rate=1e-3 \
 	  --num_steps=10 \
 	  --delta_t=0.05 \
