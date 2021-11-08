3b589aedc88f1afd09829e81fe7ff24e18a97b0c
David Fernandes <dffernandes95@gmail.com>
2021-11-08 15:40:29 +0000

    Back to biased ksd

--- stein/kernel.py
+++ stein/kernel.py
@@ -59,7 +59,7 @@ def stein_discrepancy(theta: Tensor, p_grad: Tensor, sigma: float, delta_t: Tens
     h = pairwise_dists[..., indices[0], indices[1]].median(dim=-1)[0]
     # h = torch.sqrt(
     #     sigma * h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
-    h = torch.sqrt(h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
+    h = torch.sqrt(0.001 * h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
     # h = torch.sqrt(delta_t * h).unsqueeze(-1).unsqueeze(-1)
 
     kxy = torch.exp(-pairwise_dists / h**2 / 2)
@@ -79,6 +79,6 @@ def stein_discrepancy(theta: Tensor, p_grad: Tensor, sigma: float, delta_t: Tens
     u = first_term + second_term + third_term + trace_dx2d2lxy
 
     # return torch.flatten(u, -2, -1).sum(-1) / theta.shape[-2]**2
-    u -= torch.diag_embed(torch.diagonal(u, dim1=-1, dim2=-2))
+    # u -= torch.diag_embed(torch.diagonal(u, dim1=-1, dim2=-2))
 
-    return 1 / (theta.shape[-2] * (theta.shape[-2] - 1)) * torch.abs(torch.flatten(u, -2, -1).sum(-1))
+    # return 1 / (theta.shape[-2] * (theta.shape[-2] - 1)) * torch.abs(torch.flatten(u, -2, -1).sum(-1))
