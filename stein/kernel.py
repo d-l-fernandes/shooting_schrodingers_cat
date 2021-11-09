718aef5686bd6be102cbd865d318d18a1f2ef48a
David Fernandes <dffernandes95@gmail.com>
2021-11-09 11:19:32 +0000

    Decreased number of iter

--- stein/kernel.py
+++ stein/kernel.py
@@ -11,11 +11,8 @@ def stein_discrepancy(theta: Tensor, p_grad: Tensor, sigma: float, scales: Tenso
 
     indices = torch.triu_indices(theta.shape[-2], theta.shape[-2], 1)
     h = pairwise_dists[..., indices[0], indices[1]].median(dim=-1)[0]
-    # h = torch.sqrt(
-    #     sigma * h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
     # h = torch.sqrt(h / torch.log(torch.tensor(theta.shape[-2] + 1, device=theta.device))).unsqueeze(-1).unsqueeze(-1)
-    h = torch.sqrt(h).unsqueeze(-1).unsqueeze(-1)
-    # h = torch.sqrt(delta_t * h).unsqueeze(-1).unsqueeze(-1)
+    h = torch.sqrt(h).unsqueeze(-1).unsqueeze(-1).detach()
 
     # h = torch.einsum("a...,a->a...",
     #                  h, scales)
