f1c87c63c2ac25714fa75a0d114d1c0a4145588b
David Fernandes <dffernandes95@gmail.com>
2021-11-09 12:14:38 +0000

    Variational q with independent nns for mean and std

--- sde/variational.py
+++ sde/variational.py
@@ -25,17 +25,23 @@ class Gaussian(BaseVariational):
     def __init__(self, input_size: int, output_size: int, sigma: float):
         super().__init__(input_size, output_size, sigma)
         intermediate_size = 20 * input_size
-        self.nn = torch.nn.Sequential(
+        self.mean_nn = torch.nn.Sequential(
             torch.nn.Linear(self.input_size, intermediate_size), torch.nn.SiLU(),
             torch.nn.Linear(intermediate_size, intermediate_size), torch.nn.SiLU(),
             torch.nn.Linear(intermediate_size, intermediate_size), torch.nn.SiLU(),
+            torch.nn.Linear(intermediate_size, self.output_size),
         )
-        self.mean_nn = torch.nn.Linear(intermediate_size, self.output_size)
+        self.std_nn = torch.nn.Sequential(
+            torch.nn.Linear(self.input_size, intermediate_size), torch.nn.SiLU(),
+            torch.nn.Linear(intermediate_size, intermediate_size), torch.nn.SiLU(),
+            torch.nn.Linear(intermediate_size, intermediate_size), torch.nn.SiLU(),
+            torch.nn.Linear(intermediate_size, self.output_size),
+        )
         # self.std_nn = torch.nn.Linear(intermediate_size, self.output_size**2)
-        self.std_nn = torch.nn.Linear(intermediate_size, self.output_size)
+        # self.std_nn = torch.nn.Linear(intermediate_size, self.output_size)
 
     def forward(self, x: Tensor) -> distributions.Distribution:
-        mean = self.mean_nn(self.nn(x))
+        mean = self.mean_nn(x)
         # out = torch.diag_embed(torch.ones_like(x, device=x.device) * self.sigma)
         # return distributions.MultivariateNormal(loc=mean, scale_tril=out.tril())
         # out = self.std_nn(self.nn(x))
@@ -44,7 +50,7 @@ class Gaussian(BaseVariational):
         # out = out - torch.diag_embed(diag) + (torch.diag_embed(functional.softplus(diag) + 1e-4))
         # return distributions.MultivariateNormal(loc=mean, scale_tril=out.tril())
         # out = functional.softplus(self.std_nn(self.nn(x))) + 1e-8
-        out = torch.sigmoid(self.std_nn(self.nn(x))) + 1e-8
+        out = functional.softplus(self.nn(x)) + 1e-8
         return distributions.MultivariateNormal(loc=mean, scale_tril=torch.diag_embed(out))
 
 
