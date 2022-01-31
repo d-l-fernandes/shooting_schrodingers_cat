9f2a3a80812c377d41ad7874c330da781375423c
David Fernandes <dffernandes95@gmail.com>
2022-01-31 14:22:01 +0000

    Now uses functorch's vmap

--- weak_solver/sdeint.py
+++ weak_solver/sdeint.py
@@ -6,6 +6,8 @@ from .noise import em_noise_parallel, srk_additive_noise_parallel, rossler_noise
 # from .noise import rossler_noise, em_noise, srk_additive_noise, rossler_noise_parallel
 # from .step_funs import rossler_step, em_step, srk_additive_step, rossler_step_parallel
 
+import functorch
+
 
 def integrate(sde, y0, ts, method='rossler'):
     if method == 'rossler':
@@ -37,16 +39,22 @@ def stochastic_integrate(sde, y0: torch.Tensor, ts, step, noise):
 
 def integrate_parallel_time_steps(sde, y0, ts, method='rossler'):
     if method == 'rossler':
-        step = rossler_step_parallel
+        # step = rossler_step_parallel
+        step = rossler_step
         noise = rossler_noise_parallel(y0.shape[-1], y0.shape[:-1], y0.device)
     elif method == 'em':
-        step = em_step_parallel
+        # step = em_step_parallel
+        step = em_step
         noise = em_noise_parallel(y0.shape[-1], y0.shape[:-1], y0.device)
     elif method == 'srk':
-        step = srk_additive_step_parallel
+        # step = srk_additive_step_parallel
+        step = srk_additive_step
         noise = srk_additive_noise_parallel(y0.shape[-1], y0.shape[:-1], y0.device)
     else:
         raise ValueError('Unknown method: {}'.format(method))
 
     delta_ts = ts[1:] - ts[:-1]
-    return step(y0, ts[:-1], noise(delta_ts), delta_ts, sde)
+    noise_values = noise(delta_ts)
+    step_fun = lambda y, t, n, delta: step(y, t, n, delta, sde)
+    return functorch.vmap(step_fun)(y0, ts[:-1], noise_values, delta_ts)
+    # return step(y0, ts[:-1], noise_values, delta_ts, sde)
