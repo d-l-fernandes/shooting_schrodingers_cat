ddd266cc683ef40e772955017a00e41801876018
David Fernandes <dffernandes95@gmail.com>
2022-02-07 16:50:45 +0000

    Changed how diffusion function is defined

--- sde/diffusions.py
+++ sde/diffusions.py
@@ -1,44 +1,40 @@
-import numpy as np
 import torch
-import torch.nn.functional as functional
 from absl import flags
 
 Tensor = torch.Tensor
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-flags.DEFINE_enum("diffusion", "scalar",
-                  ["scalar",
-                   ],
-                  "Diffusion to use.")
-flags.DEFINE_float("min_gamma", 0.1, lower_bound=0., help="Minimum diffusion.")
-flags.DEFINE_float("max_gamma", np.sqrt(2.), lower_bound=0., help="Maximum diffusion.")
+flags.DEFINE_float("initial_gamma", -1, lower_bound=-1, help="Minimum diffusion.")
+flags.DEFINE_float("total_gamma", -1, lower_bound=-1, help="Maximum diffusion.")
 FLAGS = flags.FLAGS
 
 
-class BaseDiffusion(torch.nn.Module):
-    def __init__(self, input_size: int, output_size: int, final_t: float):
+class Scalar(torch.nn.Module):
+    def __init__(self, input_size: int, output_size: int, final_t: float, max_diffusion: float):
         super().__init__()
         self.input_size = input_size
         self.output_size = output_size
         self.final_t = final_t
 
-    @staticmethod
-    def init_weights(m):
-        if isinstance(m, torch.nn.Linear):
-            torch.nn.init.zeros_(m.weight)
-            m.bias.data.fill(0.)
+        if FLAGS.total_gamma > 0:
+            g_max = FLAGS.total_gamma
+        else:
+            g_max = max_diffusion
 
+        if FLAGS.initial_gamma > 0:
+            a = FLAGS.initial_gamma
+            b = 4 / self.final_t * (g_max / self.final_t - a)
+        else:
+            a = g_max
+            b = 0.
 
-class Scalar(BaseDiffusion):
-    def __init__(self, input_size: int, output_size: int, final_t: float):
-        super().__init__(input_size, output_size, final_t)
-        self.g_min = FLAGS.min_gamma
-        self.g_max = FLAGS.max_gamma
-        g_diff = self.g_max - self.g_min
+        # self.gamma_t = \
+        #     lambda t: (self.g_min + 2 * g_diff * t / self.final_t) * (t < self.final_t / 2) + \
+        #               ((2 * self.g_max - self.g_min) - 2 * g_diff * t / self.final_t) * (t >= self.final_t / 2)
         self.gamma_t = \
-            lambda t: (self.g_min + 2 * g_diff * t / self.final_t) * (t < self.final_t / 2) + \
-                      ((2 * self.g_max - self.g_min) - 2 * g_diff * t / self.final_t) * (t >= self.final_t / 2 )
+            lambda t: (a + b * t) * (t < self.final_t / 2) + \
+                      (a + b * self.final_t - b * t) * (t >= self.final_t / 2)
 
     def forward(self, x: Tensor, t: Tensor) -> Tensor:
         gamma = self.gamma_t(t)
@@ -48,8 +44,3 @@ class Scalar(BaseDiffusion):
             return torch.ones_like(x, device=x.device) * gamma
         else:
             return torch.einsum("a...,a->a...", torch.ones_like(x, device=x.device), gamma)
-
-
-diffusions_dict = {
-    "scalar": Scalar,
-}
