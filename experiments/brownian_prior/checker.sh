62c90cd67797f9fae80f6800bfbdb8522b975c93
David Fernandes <dffernandes95@gmail.com>
2022-02-18 12:14:30 +0000

    Updated bash files

--- experiments/brownian_prior/checker.sh
+++ experiments/brownian_prior/checker.sh
@@ -4,20 +4,18 @@ number_gpus=$1
 visible_devices=$2
 
 hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="$visible_devices" dlf28/pytorch_lightning \
-  python -W ignore main.py \
-    --drift=score_network \
-    --diffusion=scalar \
-    --dataset=checker \
-    --prior=gaussian \
-    --prior_sde=brownian \
-    --prior_dist=gaussian \
-    --batch_size=1500 \
-    --num_epochs=40 \
-    --eval_frequency=20 \
-    --learning_rate=5e-5 \
-    --num_steps=20 \
-    --num_iter=10 \
-	  --batch_repeats=40 \
-    --sigma=0.001 \
-	  --solver=rossler \
+	python -W ignore main.py \
+	  --drift=score_network \
+	  --dataset=checker \
+	  --prior=gaussian \
+	  --prior_sde=brownian \
+	  --prior_dist=gaussian \
+	  --batch_size=1500 \
+	  --num_epochs=30 \
+	  --eval_frequency=40 \
+	  --learning_rate=1e-4 \
+	  --num_steps=20 \
+	  --num_iter=20 \
+	  --batch_repeats=20 \
+	  --initial_sigma=0.001 \
     --gpus="$number_gpus"
