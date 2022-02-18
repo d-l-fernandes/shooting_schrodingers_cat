5a756ed5a9b650d949377615769229c63f01b3b1
David Fernandes <dffernandes95@gmail.com>
2022-02-18 11:20:46 +0000

    Updated bounds bash files

--- experiments/bounds/mine_srk_steps=20.sh
+++ experiments/bounds/mine_srk_steps=20.sh
@@ -6,21 +6,17 @@ visible_devices=$2
 hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="$visible_devices" dlf28/pytorch_lightning \
 	python -W ignore main.py \
 	  --drift=score_network \
-	  --diffusion=scalar \
-	  --dataset=gaussian_5d_right \
-	  --prior=gaussian_5d_left \
+	  --dataset=gaussian_bound_right \
+	  --prior=gaussian_bound_left \
 	  --prior_sde=brownian \
 	  --prior_dist=gaussian \
 	  --batch_size=1500 \
 	  --num_epochs=30 \
-	  --eval_frequency=20 \
-	  --learning_rate=5e-5 \
-	  --num_steps=25 \
-	  --num_iter=10 \
-	  --batch_repeats=40 \
-	  --sigma=0.001 \
-	  --solver=rossler \
-    --gpus="$number_gpus" #\
-    # -p \
-    # --restore_date=2021-11-16\
-    # --restore_time=18:05
+	  --eval_frequency=40 \
+	  --learning_rate=1e-4 \
+	  --num_steps=20 \
+	  --num_iter=20 \
+	  --batch_repeats=20 \
+	  --initial_sigma=0.001 \
+	  --solver=srk \
+    --gpus="$number_gpus"
