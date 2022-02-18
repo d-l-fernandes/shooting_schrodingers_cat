3002cbf5ad1419f5ed2284e3a6a68df809e08dc6
David Fernandes <dffernandes95@gmail.com>
2022-02-18 12:23:59 +0000

    Added brownian dsb bash files

---
+++ experiments/brownian_prior_dsb/three_blobs_3d.sh
@@ -0,0 +1,22 @@
+cd ../../
+
+number_gpus=$1
+visible_devices=$2
+
+hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="$visible_devices" dlf28/pytorch_lightning \
+	python -W ignore main.py \
+	  --drift=score_network \
+	  --dataset=toy_experiment_blobs_3d \
+	  --prior=gaussian \
+	  --prior_sde=brownian \
+	  --prior_dist=gaussian \
+	  --batch_size=1500 \
+	  --num_epochs=30 \
+	  --eval_frequency=40 \
+	  --learning_rate=1e-4 \
+	  --num_steps=20 \
+	  --num_iter=20 \
+	  --batch_repeats=20 \
+	  --initial_sigma=0.001 \
+    --gpus="$number_gpus" \
+    --do_dsb
