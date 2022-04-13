9e87972286f0e0593246cb931670302cd9919bf7
David Fernandes <dffernandes95@gmail.com>
2022-04-13 12:42:59 +0100

    Stein now works with unbiased

--- stein/kernel.py
+++ stein/kernel.py
@@ -3,16 +3,18 @@ import torch
 Tensor = torch.Tensor
 
 
-def stein_discrepancy(theta: Tensor, p_grad: Tensor) -> Tensor:
+def stein_discrepancy(theta: Tensor, p_grad: Tensor, sigma) -> Tensor:
 
-    diffs = (theta.unsqueeze(-2) - theta.unsqueeze(-3)) # * delta_t**0.5
+    diffs = theta.unsqueeze(-2) - theta.unsqueeze(-3) # * delta_t**0.5
     pairwise_dists = torch.sum(diffs**2, -1)
 
     indices = torch.triu_indices(theta.shape[-2], theta.shape[-2], 1)
-    # h = pairwise_dists[..., indices[0], indices[1]].median(dim=-1)[0]
-    h = pairwise_dists[..., indices[0], indices[1]].mean(dim=-1)
+    h = pairwise_dists[..., indices[0], indices[1]].median(dim=-1)[0]
+    # h = pairwise_dists[..., indices[0], indices[1]].mean(dim=-1)
     h = torch.sqrt(h).unsqueeze(-1).unsqueeze(-1).detach() # / np.log(theta.shape[-2] + 1)
 
+    # h = sigma * h
+    # h = h * 0. + 1.
     kxy = torch.exp(-pairwise_dists / h**2 / 2)
 
     h = h.unsqueeze(-1)
@@ -25,11 +27,9 @@ def stein_discrepancy(theta: Tensor, p_grad: Tensor) -> Tensor:
 
     u = first_term + second_term + third_term + trace_dx2d2kxy
 
-    return torch.flatten(u, -2, -1).sum(-1) / theta.shape[-2]**2
-    # u -= torch.diag_embed(torch.diagonal(u, dim1=-1, dim2=-2))
+    # return torch.flatten(u, -2, -1).sum(-1) / theta.shape[-2]**2
+    u -= torch.diag_embed(torch.diagonal(u, dim1=-1, dim2=-2))
 
-    # return 1 / (theta.shape[-2] * (theta.shape[-2] - 1)) * torch.abs(torch.flatten(u, -2, -1).sum(-1))
-    # return \
-    #     torch.sqrt(1 / (theta.shape[-2] * (theta.shape[-2] - 1)) * torch.square(torch.flatten(u, -2, -1)).sum(-1))
+    return 1 / (theta.shape[-2] * (theta.shape[-2] - 1)) * torch.flatten(u, -2, -1).sum(-1)
 
 
