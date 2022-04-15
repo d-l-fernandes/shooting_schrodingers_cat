45776e07916793583b8c9cce97c867b795fabafb
David Fernandes <dffernandes95@gmail.com>
2022-04-15 11:06:54 +0100

    Updated previous commit

--- stein/kernel.py
+++ stein/kernel.py
@@ -26,15 +26,31 @@ def stein_discrepancy(theta: Tensor, p_grad: Union[Tensor, Tuple[Tensor]],
     dxdkxy = - 1 / h**2 * torch.einsum("...bcd,...bc->...bcd", diffs, kxy)
     trace_dx2d2kxy = torch.einsum("...a,...->...a", -1 / h**4 * diffs**2 + 1 / h**2, kxy).sum(-1)
 
-    first_term = torch.einsum("...ab,...ac,...cb->...ac", p_grad, kxy, p_grad)
-    second_term = torch.einsum("...ab,...acb->...ac", p_grad, -dxdkxy)
-    third_term = torch.einsum("...acb,...cb->...ac", dxdkxy, p_grad)
+    if type(p_grad) is not tuple:
+        first_term = torch.einsum("...ab,...ac,...cb->...ac", p_grad, kxy, p_grad)
+        second_term = torch.einsum("...ab,...acb->...ac", p_grad, -dxdkxy)
+        third_term = torch.einsum("...acb,...cb->...ac", dxdkxy, p_grad)
 
-    u = first_term + second_term + third_term + trace_dx2d2kxy
+        u = first_term + second_term + third_term + trace_dx2d2kxy
 
-    # return torch.flatten(u, -2, -1).sum(-1) / theta.shape[-2]**2
-    u -= torch.diag_embed(torch.diagonal(u, dim1=-1, dim2=-2))
+        return torch.flatten(u, -2, -1).sum(-1) / theta.shape[-2]**2
+        # u -= torch.diag_embed(torch.diagonal(u, dim1=-1, dim2=-2))
 
-    return 1 / (theta.shape[-2] * (theta.shape[-2] - 1)) * torch.flatten(u, -2, -1).sum(-1)
+        # return 1 / (theta.shape[-2] * (theta.shape[-2] - 1)) * torch.flatten(u, -2, -1).sum(-1)
+    else:
+        first_terms =  \
+            (torch.einsum("...ab,...ac,...cb->...ac", i, kxy, i) 
+            for i in p_grad)
+        second_terms = \
+            (torch.einsum("...ab,...acb->...ac", i, -dxdkxy) 
+            for i in p_grad)
+        third_terms = \
+            (torch.einsum("...acb,...cb->...ac", dxdkxy, i)
+            for i in p_grad)
+
+        us = (sum(i) for i in zip(first_terms, second_terms, third_terms))
+
+        return (torch.flatten(i, -2, -1).sum(-1) / theta.shape[-2]**2 
+                for i in us)
 
 
