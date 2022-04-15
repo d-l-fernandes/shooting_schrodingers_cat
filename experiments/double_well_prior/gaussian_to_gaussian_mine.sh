8fd64f6c5255f034857e22d17b52ee1999ad3efc
David Fernandes <dffernandes95@gmail.com>
2022-04-15 11:00:46 +0100

    Changed parameters

--- experiments/double_well_prior/gaussian_to_gaussian_mine.sh
+++ experiments/double_well_prior/gaussian_to_gaussian_mine.sh
@@ -6,9 +6,9 @@ visible_devices=$2
 hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="$visible_devices" dlf28/pytorch_lightning \
 	python -W ignore main.py \
 	  --drift=nn_general \
-    --dataset=double_well_right \
-    --prior=double_well_left \
-    --prior_sde=hill \
+      --dataset=double_well_right \
+      --prior=double_well_left \
+      --prior_sde=hill \
 	  --prior_dist=gaussian \
 	  --batch_size=1500 \
 	  --num_epochs=25 \
