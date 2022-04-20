f4a2dae36b192bd6b485bafd8d1fd2ac74c84476
David Fernandes <dffernandes95@gmail.com>
2022-04-20 14:23:26 +0100

    Changed parameters

--- experiments/double_well_prior/mine.sh
+++ experiments/double_well_prior/mine.sh
@@ -3,8 +3,6 @@ cd ../../
 number_gpus=$1
 visible_devices=$2
 gamma=$3
-ksd_scale=$4
-uniform_delta_t=$5
 
 hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="$visible_devices" dlf28/pytorch_lightning \
 	python -W ignore main.py \
@@ -15,12 +13,9 @@ hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime
 	  --prior_dist=gaussian \
 	  --batch_size=1500 \
 	  --num_epochs=15 \
-	  --learning_rate=2e-4 \
+	  --learning_rate=5e-4 \
 	  --num_steps=25 \
 	  --sigma=1e-3 \
 	  --total_gamma="$gamma" \
 	  --normalize=False \
-	  --solver=srk \
       --gpus="$number_gpus" \
-      --uniform_delta_t="$uniform_delta_t" \
-	  --ksd_scale="$ksd_scale"
