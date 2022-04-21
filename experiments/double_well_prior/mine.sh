52ee5de3e8c62956355669cedf8c3d38c730da25
David Fernandes <dffernandes95@gmail.com>
2022-04-21 19:04:47 +0100

    Changed parameters

--- experiments/double_well_prior/mine.sh
+++ experiments/double_well_prior/mine.sh
@@ -2,20 +2,20 @@ cd ../../
 
 number_gpus=$1
 visible_devices=$2
-gamma=$3
 
 hare run --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g) --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="$visible_devices" dlf28/pytorch_lightning \
 	python -W ignore main.py \
-	  --drift=nn_general \
+	  --drift=score_network \
       --dataset=double_well_right \
       --prior=double_well_left \
       --prior_sde=hill \
 	  --prior_dist=gaussian \
 	  --batch_size=1500 \
-	  --num_epochs=15 \
-	  --learning_rate=5e-4 \
+	  --num_epochs=20 \
+	  --learning_rate=1e-3 \
+	  --schedule_iter=5 \
 	  --num_steps=25 \
 	  --sigma=1e-3 \
-	  --total_gamma="$gamma" \
+	  --total_gamma=1. \
 	  --normalize=False \
-      --gpus="$number_gpus" \
+      --gpus="$number_gpus"
--- experiments/double_well_prior/run_gaussian_to_gaussian_mine.sh
+++ experiments/double_well_prior/run_gaussian_to_gaussian_mine.sh
@@ -1,6 +1,4 @@
 number_gpus=$1
 visible_devices=$2
 
-# bash mine.sh "$number_gpus" "$visible_devices" 0.5
-bash mine.sh "$number_gpus" "$visible_devices" 1.0
-bash mine.sh "$number_gpus" "$visible_devices" 1.5
+bash mine.sh "$number_gpus" "$visible_devices"
